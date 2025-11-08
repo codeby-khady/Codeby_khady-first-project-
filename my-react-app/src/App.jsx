bf33ed84ca3bf8a54329839bfa334859e574595d
Khadija Muhammad Albarnawy <khadijamalbarnawy12@gmail.com>
2025-11-08 05:52:04 +0000

    My first React project completed

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+    return (
+        <div
+              style={{
+                      display: "flex",
+                              justifyContent: "center",
+                                      alignItems: "center",
+                                              height: "100vh",
+                                                      fontWeight: "bold",
+                                                              fontSize: "24px",
+                                                                    }}
+                                                                        >
+                                                                              HELLO THIS IS MY FIRST PROJECT
+                                                                                  </div>
+                                                                                    );
+                                                                                    }
+
+                                                                                    export default App;
+
